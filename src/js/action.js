4057b6d1fb546cb466728b288d8b3c5b50b8980c
Collins Tatang <makungongcollins@gmail.com>
2021-07-28 16:56:20 +0100

    Add the action file

---
+++ src/js/action.js
@@ -0,0 +1,14 @@
+const topScores = (scores) => scores.sort((a, b) => {
+  if (+a.score > +b.score) return -1;
+  return 1;
+}); 
+
+const refresh = () => {
+  window.location.reload();
+};
+
+const clearFields = () => {
+  document.querySelector('.name').value = '';
+  document.querySelector('.score').value = '';
+};
+export { topScores, refresh, clearFields };
