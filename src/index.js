adf27e91ae20b8f5f9db5bc3be1b06324ccafdf1
Collins Tatang <makungongcollins@gmail.com>
2021-07-28 20:01:12 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -2,43 +2,43 @@ import { getScores, postScore } from './asset/api';
 import { topScores, refresh, clearFields } from './asset/action';
 import './style.css';
 
-const body = document.querySelector('body');
-body.innerHTML = ` 
+// const body = document.querySelector('body');
+// body.innerHTML = ` 
 
-<div class="container mt-3">
-    <div class=" h1 font-weight-bold">Leaderboard</div>
-    <div class="row"> 
-    <div class="inner-container d-flex col-md-6">
-      <div class="scores-container">
-        <div class="d-flex">
-          <div class="h4 font-weight-bold">Recent scores</div>
-          <button type="button" class="btn btn-primary btn-sm btn-refresh"> Refresh</button>
-        </div>
-        <div class="">
-          <table class="table table-striped mt-4">
-            <thead class="thead-dark">
-                <tr>
-                  <th class="display-5 lead text-white">Name</th>
-                  <th class="display-5 lead text-white">Scores</th>                 
-                </tr>
-            </thead>
-            <tbody id="boardList">
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div class="form-container">
-        <div class="h4 font-weight-bold">Add your score</div>
-        <form action="#" class="d-flex form">
-          <input type="text" class="name form-control mb-4" placeholder="Enter name">
-          <input type="number" class="score form-control mb-4" placeholder="Enter score">
-          <input type="submit" value="submit" id="submit" class="btn btn-success btn-sm">
-        </form>
-      </div>
-    </div>
-    </div>
-  </div>
-`;
+// <div class="container mt-3">
+//     <div class=" h1 font-weight-bold">Leaderboard</div>
+//     <div class="row"> 
+//     <div class="inner-container d-flex col-md-6">
+//       <div class="scores-container">
+//         <div class="d-flex">
+//           <div class="h4 font-weight-bold">Recent scores</div>
+//           <button type="button" class="btn btn-primary btn-sm btn-refresh"> Refresh</button>
+//         </div>
+//         <div class="">
+//           <table class="table table-striped mt-4">
+//             <thead class="thead-dark">
+//                 <tr>
+//                   <th class="display-5 lead text-white">Name</th>
+//                   <th class="display-5 lead text-white">Scores</th>                 
+//                 </tr>
+//             </thead>
+//             <tbody id="boardList">
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>
+//       <div class="form-container">
+//         <div class="h4 font-weight-bold">Add your score</div>
+//         <form action="#" class="d-flex form">
+//           <input type="text" class="name form-control mb-4" placeholder="Enter name">
+//           <input type="number" class="score form-control mb-4" placeholder="Enter score">
+//           <input type="submit" value="submit" id="submit" class="btn btn-success btn-sm">
+//         </form>
+//       </div>
+//     </div>
+//     </div>
+//   </div>
+// `;
 
 const leaderContent = document.querySelector('#boardList');
 const btn = document.querySelector('#submit');
